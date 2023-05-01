6390715db0185da1335152c3fffeb7ac5abf159b
Leonardo <lupadinho867@gmail.com>
2023-05-01 07:40:59 -0300

    atualizacao do projeto

--- js/main.js
+++ js/main.js
@@ -3,4 +3,6 @@ $(document).ready(function() {
     $('#campo-cpf').mask('000.000.000-00',{reverse: true});
 
     $('#campo-cep').mask('00000-000');
+
+    $('#telefone').mask('(00)00000-0000');
 })
